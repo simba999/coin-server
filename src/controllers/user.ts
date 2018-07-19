8b2341d25e9248562cee91882e37dbc360cbf098
simba999 <simba1512@yahoo.com>
2018-07-19 10:13:53 +0200

    dev(core): IS-1 authentication backend API(not completed)

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -98,4 +98,15 @@ router.get('/me',
     })
 );
 
+/**
+ *
+ */
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
 export default router;
